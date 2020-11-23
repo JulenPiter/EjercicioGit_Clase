521768354055b5eb910c4c17e7d125aa011a14e1
JulenPiter <48485395+JulenPiter@users.noreply.github.com>
2020-11-23 16:38:39 +0100

    Add files via upload

---
+++ EjercicioClaseGit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejercicio_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
